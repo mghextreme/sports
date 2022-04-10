bfa764bb98b1e710aa54a16574b92cc4a6cada78
Matias Henschel <matias.henschel@philips.com>
2022-04-10 09:46:07 -0300

    Added Manage event page listings and form.

--- app/src/app/pages/manage.event/manage.event.page.ts
+++ app/src/app/pages/manage.event/manage.event.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IEvent, IGroup, IModality } from 'src/app/models';
+import { AuthService, EventsService, GroupsService, ModalitiesService } from 'src/app/services';
 
 @Component({
   templateUrl: './manage.event.page.html',
@@ -8,13 +9,34 @@ import { AuthService } from 'src/app/services';
 })
 export class ManageEventPageComponent {
 
+  event: IEvent;
+  modalities: IModality[];
+  groups: IGroup[];
+
   constructor(
     readonly authService: AuthService,
+    private readonly eventsService: EventsService,
+    private readonly activeRoute: ActivatedRoute,
     private router: Router
   ) {
     if (!authService.isLoggedIn) {
       this.router.navigate(['/login']);
     }
+
+    this.event = this.eventsService.getDefault();
+    this.modalities = [];
+    this.groups = [];
+
+    const eventId = this.activeRoute.snapshot.params['id'];
+    this.eventsService.get(eventId).subscribe(event => {
+      this.event = event;
+    });
+    this.eventsService.getModalities(eventId).subscribe(modalities => {
+      this.modalities = modalities;
+    });
+    this.eventsService.getGroups(eventId).subscribe(groups => {
+      this.groups = groups;
+    });
   }
 
 }
