2e1b91c72169d38f5b76105411de48af82ce1999
mghextreme <matiashenschel@hotmail.com>
2022-04-19 17:50:21 -0300

    Use PrimeNG for tables in Manage Modality page.

--- app/src/app/pages/manage.modality/manage.modality.page.ts
+++ app/src/app/pages/manage.modality/manage.modality.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ConfirmationService } from 'primeng/api';
+import { Table } from 'primeng/table';
 import { IModality, ISport, IStage, ITeam } from 'src/app/models';
 import { AuthService, ModalitiesService } from 'src/app/services';
 
@@ -11,15 +13,22 @@ export class ManageModalityPageComponent {
 
   isNew = false;
 
+  isLoadingStages = true;
+  isLoadingTeams = true;
+
   modality: IModality;
   stages: IStage[];
   teams: ITeam[];
   sports: ISport[];
 
+  @ViewChild('stagesTable') stagesTable?: Table;
+  @ViewChild('teamsTable') teamsTable?: Table;
+
   constructor(
     readonly authService: AuthService,
     private readonly modalitiesService: ModalitiesService,
     private readonly activeRoute: ActivatedRoute,
+    private readonly confirmationService: ConfirmationService,
     private router: Router
   ) {
     if (!authService.isLoggedIn) {
@@ -45,11 +54,57 @@ export class ManageModalityPageComponent {
       });
       this.modalitiesService.getStages(modalityId).subscribe(stages => {
         this.stages = stages;
+        this.isLoadingStages = false;
       });
       this.modalitiesService.getTeams(modalityId).subscribe(teams => {
         this.teams = teams;
+        this.isLoadingTeams = false;
       });
     }
+  }
+
+  filterStagesTable(event: Event) {
+    const htmlTarget = event.target as HTMLTextAreaElement;
+    this.stagesTable?.filterGlobal(htmlTarget.value, 'contains');
+  }
+
+  openStage(stage: IStage) {
+    // Modal
+  }
+
+  removeStage(stage: IStage) {
+    this.confirmationService.confirm({
+        message: 'Are you sure you want to remove this stage?',
+        header: 'Remove Name of Stage',
+        acceptButtonStyleClass: 'p-button-danger',
+        rejectButtonStyleClass: 'p-button-secondary',
+        accept: () => {
+          // TODO
+          console.error('removed');
+        }
+    });
+  }
+
+  filterTeamsTable(event: Event) {
+    const htmlTarget = event.target as HTMLTextAreaElement;
+    this.teamsTable?.filterGlobal(htmlTarget.value, 'contains');
+  }
+
+  openTeam(team: ITeam) {
+    // Modal
+  }
+
+  removeTeam(team: ITeam) {
+    this.confirmationService.confirm({
+        message: 'Are you sure you want to remove this team?',
+        header: 'Remove Name of Team',
+        acceptButtonStyleClass: 'p-button-danger',
+        rejectButtonStyleClass: 'p-button-secondary',
+        accept: () => {
+          // TODO
+          console.error('removed');
+        }
+    });
   }
 
 }
